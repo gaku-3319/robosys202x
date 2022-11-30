a0f98d33a88327879f1bf45867e55ef2d46b9e19
Gakuto Seyama <gakusan_0729@icloud.com>
2022-11-30 16:56:55 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clu
+ng () {
+      echo NG at Line $1
+      res=1
+}
 
+res=0
+
+### I/O TEST###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK
+exit $res
