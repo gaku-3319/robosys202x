246a32b8ed006d6771621f1b2416d197e214dbc5
Gakuto Seyama <gakusan_0729@icloud.com>
2022-11-30 17:19:49 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clu
+
 ng () {
       echo NG at Line $1
       res=1
@@ -12,5 +13,11 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
+
